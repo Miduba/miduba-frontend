7799118b2f24d1e2e5f4cf78fdeeda6ab4b96fdb
이도현 <ksmfou98@naver.com>
2022-03-02 19:47:19 +0900

    :sparkles: implements media query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
+import styled from "@emotion/styled";
 import Button from "components/Button";
 import Header from "components/Header";
 import type { NextPage } from "next";
+import media from "styles/media";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <Test>
       <Header />
       <main>
         Home
@@ -17,8 +19,22 @@ const Home: NextPage = () => {
           Hello
         </Button>
       </main>
-    </>
+    </Test>
   );
 };
+
+// @Note 아래는 반응형 예시 코드 입니다 확인 후 삭제해주세요~
+const Test = styled.div`
+  background-color: red;
+  ${media.xlarge} {
+    background-color: blue;
+  }
+  ${media.large} {
+    background-color: green;
+  }
+  ${media.medium} {
+    background-color: yellow;
+  }
+`;
 
 export default Home;
